54439a8485e049c48847d41999a6d40e5f2d89d5
kodonghyun <kodonghyun1209@gmail.com>
2017-08-31 20:38:16 +0900

    내 지역 디비 추가

--- app/src/main/java/com/waterdrop/k/waterdrop/DataBase/MyRegion.java
+++ app/src/main/java/com/waterdrop/k/waterdrop/DataBase/MyRegion.java
@@ -1,8 +1,23 @@
 package com.waterdrop.k.waterdrop.DataBase;
 
-/**
- * Created by kodong on 2017. 8. 31..
- */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class MyRegion {
+public class MyRegion extends SQLiteOpenHelper {
+    public MyRegion(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        // 테이블 생성
+        database.execSQL("CREATE TABLE myregion (_id INTEGER PRIMARY KEY AUTOINCREMENT, region_id INTEGER);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        database.execSQL("DROP TABLE IF EXISTS myregion");
+        onCreate(database);
+    }
 }
