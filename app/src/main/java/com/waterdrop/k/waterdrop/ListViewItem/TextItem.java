74496bde0cc4cdfebf86baeb6059fdfdfc4deac4
kodonghyun <kodonghyun1209@gmail.com>
2017-08-31 23:36:23 +0900

    텍스트 관련 어댑터 및 아이템 클래스 추가

---
+++ app/src/main/java/com/waterdrop/k/waterdrop/ListViewItem/TextItem.java
@@ -0,0 +1,22 @@
+package com.waterdrop.k.waterdrop.ListViewItem;
+
+public class TextItem {
+    private long id;
+    private String text;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
